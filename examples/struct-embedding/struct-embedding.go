2f967e1232fdac9801a3d7d0e29fff0e97441018
jennychiang <jennyc201911@gmail.com>
2023-08-12 11:00:40 +0800

    add: This commit adds some comments about the usage of struct-embedding

--- examples/struct-embedding/struct-embedding.go
+++ examples/struct-embedding/struct-embedding.go
@@ -12,12 +12,17 @@ type base struct {
 	num int
 }
 
+// 為 base 結構定義了一個方法 describe()。
+// 方法在其名稱之前有一個特殊的參數（在此稱為接收器），該參數表示該方法作用的具體類型。
+// 在這個例子中，describe() 的接收器是 (b base)，這意味著 describe() 是 base 類型的方法。
 func (b base) describe() string {
+	// %v 是一個占位符，它將被後面參數的值替換。
 	return fmt.Sprintf("base with num=%v", b.num)
 }
 
 // A `container` _embeds_ a `base`. An embedding looks
 // like a field without a name.
+// 定義 container 結構體並嵌入 base
 type container struct {
 	base
 	str string
@@ -28,6 +33,7 @@ func main() {
 	// When creating structs with literals, we have to
 	// initialize the embedding explicitly; here the
 	// embedded type serves as the field name.
+	// 創建一個 container 的實例，並明確初始化其嵌入的 base。
 	co := container{
 		base: base{
 			num: 1,
@@ -37,6 +43,7 @@ func main() {
 
 	// We can access the base's fields directly on `co`,
 	// e.g. `co.num`.
+	// 直接從 container 的實例 co 訪問 base 的 num 字段，而不需要使用中間的 base 字段名。
 	fmt.Printf("co={num: %v, str: %v}\n", co.num, co.str)
 
 	// Alternatively, we can spell out the full path using
@@ -47,6 +54,8 @@ func main() {
 	// `base` also become methods of a `container`. Here
 	// we invoke a method that was embedded from `base`
 	// directly on `co`.
+	// 由於 container 嵌入了 base，所以 base 的方法也變成了 container 的方法。
+	// 因此，可以直接在 co 上調用 describe 方法，這實際上是從 base 嵌入的。
 	fmt.Println("describe:", co.describe())
 
 	type describer interface {
@@ -57,6 +66,8 @@ func main() {
 	// interface implementations onto other structs. Here
 	// we see that a `container` now implements the
 	// `describer` interface because it embeds `base`.
+	// 由於 container 嵌入了具有 describe 方法的 base，container 也被視為實現了 describer 介面。
+	// 可以通過嵌入來重用或組合功能，而不需要明確地在每個結構上實現方法。
 	var d describer = co
 	fmt.Println("describer:", d.describe())
 }
