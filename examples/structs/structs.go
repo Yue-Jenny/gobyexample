acd279f274046ea26b322313d77239920550741b
jennychiang <jennyc201911@gmail.com>
2023-08-12 09:44:06 +0800

    add: This commit adds some comments about the usage of structs

--- examples/structs/structs.go
+++ examples/structs/structs.go
@@ -7,6 +7,10 @@ package main
 import "fmt"
 
 // This `person` struct type has `name` and `age` fields.
+// 結構體是一種複合的數據類型，用來組合多種不同（或相同）的數據類型到一個單一的類型中。
+// type 關鍵字表示我們要定義一個新的類型，person 是這個新類型的名稱，而 struct 告訴 Go，這個新類型將是一個結構體。
+// name string: 這表示 person 結構體有一個名為 name 的字段，其數據類型為 string。
+// age int: 這表示 person 結構體有一個名為 age 的字段，其數據類型為 int
 type person struct {
 	name string
 	age  int
@@ -16,6 +20,7 @@ type person struct {
 func newPerson(name string) *person {
 	// You can safely return a pointer to local variable
 	// as a local variable will survive the scope of the function.
+	// 創建 person 的實例（或稱為對象），並給它賦值，
 	p := person{name: name}
 	p.age = 42
 	return &p
@@ -33,6 +38,7 @@ func main() {
 	fmt.Println(person{name: "Fred"})
 
 	// An `&` prefix yields a pointer to the struct.
+	// &: 此符號是取地址運算符。返回該變量的內存地址
 	fmt.Println(&person{name: "Ann", age: 40})
 
 	// It's idiomatic to encapsulate new struct creation in constructor functions
@@ -44,6 +50,7 @@ func main() {
 
 	// You can also use dots with struct pointers - the
 	// pointers are automatically dereferenced.
+	// 在 Go 語言中，當你有一個指針指向某個結構時，你可以直接使用 . 來訪問其欄位，而不需要明確的解參考。
 	sp := &s
 	fmt.Println(sp.age) // Output: 50
 
@@ -55,6 +62,7 @@ func main() {
 	// have to give it a name. The value can have an anonymous
 	// struct type. This technique is commonly used for
 	// [table-driven tests](testing-and-benchmarking).
+	// 定義了一個匿名結構體，並且初始化匿名結構體
 	dog := struct {
 		name   string
 		isGood bool
